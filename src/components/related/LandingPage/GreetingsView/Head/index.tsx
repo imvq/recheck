7a27fdd9ea1e35048f31ab629a759285c4bcfa21
Cirill Usachov <usachov@imvq.space>
2021-02-27 17:40:55 +0200

    Logo SVG provided + greetings view head customized

--- src/components/related/LandingPage/GreetingsView/Head/index.tsx
+++ src/components/related/LandingPage/GreetingsView/Head/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import withStyles from 'react-css-modules';
 
+import logoFull from 'assets/svg/logoFull.svg';
 import styles from './index.module.scss';
 
 /**
@@ -8,7 +9,11 @@ import styles from './index.module.scss';
  * Presentational component.
  */
 const Head: React.FC = () => (
-  <div styleName='Head' />
+  <div styleName='Head'>
+    <img styleName='Head__Logo' src={logoFull} alt='' />
+    <div><span>tabtabtab</span></div>
+    <div><button type='button'>telega</button></div>
+  </div>
 );
 
 export default withStyles(Head, styles);
