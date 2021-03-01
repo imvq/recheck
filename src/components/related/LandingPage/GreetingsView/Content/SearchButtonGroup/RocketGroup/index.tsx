2b337f3867da3123528fe0710a4349f984a32c1d
Cirill Usachov <usachov@imvq.space>
2021-03-02 00:20:45 +0200

    Minor stuff

--- src/components/related/LandingPage/GreetingsView/Content/SearchButtonGroup/RocketGroup/index.tsx
+++ src/components/related/LandingPage/GreetingsView/Content/SearchButtonGroup/RocketGroup/index.tsx
@@ -4,6 +4,10 @@ import { ReactComponent as LineSVG } from 'assets/svg/misc/RocketLine.svg';
 import Rocket from 'assets/images/Rocket.png';
 import styles from './styles.module.scss';
 
+/**
+ * Rocket and its path (represented as a separate object).
+ * Presentational component.
+ */
 export default withStyles(() => (
   <div>
     <LineSVG styleName='RocketGroup__Line' />
