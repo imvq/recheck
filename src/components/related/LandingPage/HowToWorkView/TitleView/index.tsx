c879cc807ba37dc5270bff45baae90ca2ee4bc55
Cirill Usachov <usachov@imvq.space>
2021-03-02 18:38:20 +0200

    'How to work' section styles and structure updated

--- src/components/related/LandingPage/HowToWorkView/TitleView/index.tsx
+++ src/components/related/LandingPage/HowToWorkView/TitleView/index.tsx
@@ -2,6 +2,19 @@ import withStyles from 'react-css-modules';
 
 import styles from './styles.module.scss';
 
+/**
+ * Title of 'How to work' section.
+ * Prsentational component.
+ */
 export default withStyles(() => (
-  <div styleName='TitleView' />
+  <div styleName='TitleView'>
+    <div styleName='TitleView__TextWrapper'>
+      <span styleName='TitleView__Text'>
+        <span>Как работает </span>
+        <span styleName='TitleView__Text--ColorpickMain'>re</span>
+        <span styleName='TitleView__Text--ColorpickAux1'>Check</span>
+        <span>?</span>
+      </span>
+    </div>
+  </div>
 ), styles);
