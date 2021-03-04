3abe04060423ffa2736091d526c37e4e4cada290
Cirill Usachov <usachov@imvq.space>
2021-03-04 16:11:41 +0200

    Infoblock completed

--- src/components/related/LandingPage/InfoblockView/InfoEasement/index.tsx
+++ src/components/related/LandingPage/InfoblockView/InfoEasement/index.tsx
@@ -4,9 +4,25 @@ import styles from './styles.module.scss';
 
 export default withStyles(() => (
   <div styleName='InfoEasement'>
-    <div>
-      <h1>Local Title</h1>
-      <p>Lorem Ipsum</p>
+    <div styleName='InfoEasement__BoardWrapper'>
+      <div styleName='InfoEasement__Board'>
+        <div styleName='InfoEasement__ParagraphWrapper'>
+          <div styleName='InfoEasement__TitleWrapper'>
+            <h1 styleName='InfoEasement__Title'>
+              <span>
+                Можно проверять кадидата
+                ещё до того, как он придёт
+                на собеседование.
+              </span>
+            </h1>
+          </div>
+          <p styleName='InfoEasement__Text'>
+            Теперь не обязательно ждать
+            финальное собеседование,
+            чтобы получить рекомендацию.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 ), styles);
