5985485b1698df14f2c3a2639a8355dd28237f38
Cirill Usatchoff <usatchoff@gmail.com>
2021-03-12 12:59:45 +0200

    Motivator view implemented

--- src/components/related/LandingPage/MotivatorView/index.tsx
+++ src/components/related/LandingPage/MotivatorView/index.tsx
@@ -1,5 +1,7 @@
 import withStyles from 'react-css-modules';
 
+import { ReactComponent as FindButton } from 'assets/svg/btn/FindCandidate.svg';
+import PhoneImage from 'assets/images/misc/Phone.png';
 import TitleView from './TitleView';
 import Badge from './Badge';
 import styles from './styles.module.scss';
@@ -7,6 +9,22 @@ import styles from './styles.module.scss';
 export default withStyles(() => (
   <div styleName='MotivatorView'>
     <TitleView />
-    <Badge text='Проверяйте отзывы о кандидате с прошлого места работы.' />
+    <div styleName='MotivatorView__FocusView'>
+      <div styleName='MotivatorView__Anchor'>
+        <div styleName='MotivatorView__MotivationBadgeWrapper'>
+          <Badge text='Проверяйте отзывы о кандидате с прошлого места работы.' />
+        </div>
+        <div styleName='MotivatorView__MotivationBadgeWrapper'>
+          <Badge text='Нанимайте подходящих кандидатов.' />
+        </div>
+        <div styleName='MotivatorView__MotivationBadgeWrapper'>
+          <Badge text='Обменивайтесь отзывами о своих сотрудниках.' />
+        </div>
+      </div>
+      <div styleName='MotivatorView__Anchor--sticky'>
+        <img styleName='MotivatorView__PhoneImage' src={PhoneImage} alt='' />
+        <FindButton styleName='MotivatorView__Button' />
+      </div>
+    </div>
   </div>
 ), styles);
