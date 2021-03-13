819fb67870bf9f1f1777d0e1b9a9210e285829a0
Cirill Usatchoff <usatchoff@gmail.com>
2021-03-13 16:17:42 +0200

    Search popup layout implemented

--- src/components/common/Popups/SearchPopup/index.tsx
+++ src/components/common/Popups/SearchPopup/index.tsx
@@ -1,5 +1,7 @@
 import withStyles from 'react-css-modules';
 
+import SearchStage1Pic from 'assets/images/misc/SearchStage1Pic.png';
+import { ReactComponent as FindButton } from 'assets/svg/btn/FindCandidate.svg';
 import { SearchPopupProps } from './types';
 import styles from './styles.module.scss';
 
@@ -9,7 +11,16 @@ import styles from './styles.module.scss';
  */
 export default withStyles(({ onClose }: SearchPopupProps) => (
   <div styleName='SearchPopup'>
-    <div styleName='SearchPopup__Frame' />
+    <div styleName='SearchPopup__Frame'>
+      <div styleName='SearchPopup__StagePicWrapper'>
+        <img styleName='SearchPopup__StagePic' src={SearchStage1Pic} alt='' />
+      </div>
+      <div styleName='SearchPopup__ContentWrapper'>
+        <h1 styleName='SearchPopup__Header'>Поиск кандидата</h1>
+        <input styleName='SearchPopup__Input' type='text' placeholder='Ссылка на LinkedIn' />
+        <FindButton styleName='SearchPopup__Button' />
+      </div>
+    </div>
     <div
       styleName='SearchPopup__ClickableBackground'
       onClick={onClose}
