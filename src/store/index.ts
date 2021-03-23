65692e26d762528b1849a6cfcad7e4d298947e8b
Cirill Usatchoff <usatchoff@gmail.com>
2021-03-23 23:25:26 +0200

    Redux Thunk middleware included

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,43 @@
-import { combineReducers, createStore } from 'redux';
+import thunkMiddlware, { ThunkMiddleware } from 'redux-thunk';
+import {
+  combineReducers, createStore, applyMiddleware, compose
+} from 'redux';
 
 import { authReducer } from './auth/reducers';
 import { interactionStateReducer } from './interaction/reducers';
+import { AppActionType } from './types';
 
+/**
+ * All store reducers combined to one.
+ */
 const rootReducer = combineReducers({
   auth: authReducer,
   interaction: interactionStateReducer
 });
 
+/**
+ * Application of all the middlewares.
+ * If the redux extension is installed, then use its code
+ * (which provides functionality of redux compose function
+ * + additional stuff for debug and state tracking).
+ * Otherwise use default redux compose function.
+ */
+const composeEnhancers = (window as any)
+  .__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+/**
+ * Type for root reducer. To be used in connections.
+ */
 export type AppState = ReturnType<typeof rootReducer>;
+
+/**
+ * Store object encapsulating all app state.
+ */
 export const store = createStore(
   rootReducer,
-  // eslint-disable-next-line no-underscore-dangle
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__
-  // eslint-disable-next-line no-underscore-dangle
-  && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(
+    thunkMiddlware as ThunkMiddleware<AppState, AppActionType>
+  ))
 );
 
 export * from './auth/actions';
