82ba6f81870111744de0fa40f217815240d5cf83
Cirill Usatchoff <usatchoff@gmail.com>
2021-03-30 20:36:06 +0300

    Cookie popup styles updated

--- src/components/reusables/CookiePopup/index.tsx
+++ src/components/reusables/CookiePopup/index.tsx
@@ -1,8 +1,25 @@
-import { Wrapper } from './styled';
+import CookieImage from 'assets/images/reusables/CookiePopup/Cookie.png';
+import {
+  Wrapper, SubsectionWrapper, AdaptedImage, TextWrapper, StyledLink, LinkButton
+} from './styled';
 
 /**
  * Popup with warning about cookies.
  */
 export default () => (
-  <Wrapper />
+  <Wrapper>
+    <SubsectionWrapper>
+      <AdaptedImage src={CookieImage} draggable={false} />
+      <TextWrapper>
+        <span>
+          Этот сайт использует cookies. Вы можете изменить настройки cookies в
+          своём браузере.&nbsp;&nbsp;
+        </span>
+        <StyledLink to='/about'>Узнать больше</StyledLink>
+      </TextWrapper>
+    </SubsectionWrapper>
+    <SubsectionWrapper>
+      <LinkButton>Принять и закрыть</LinkButton>
+    </SubsectionWrapper>
+  </Wrapper>
 );
--- src/components/reusables/CookiePopup/styled.tsx
+++ src/components/reusables/CookiePopup/styled.tsx
@@ -1,12 +1,73 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import { cssVars } from 'style.common';
 
+/**
+ * Styled component for main wrapper of cookie popup.
+ */
 export const Wrapper = styled.div`
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: 12rem;
+  height: 9rem;
   background-color: #e1e6fb;
   z-index: ${cssVars.zIndexPopupCookie};
+  border-top: .3rem solid #9ea9d1;
+  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 1rem 5rem;
+`;
+
+/**
+ * Styled component for cookie popup subsection wrapper.
+ */
+export const SubsectionWrapper = styled.section`
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  &:last-child {
+    justify-content: flex-end;
+  }
+`;
+
+/**
+ * Styled component for the main popup image.
+ */
+export const AdaptedImage = styled.img`
+  height: 80%;
+`;
+
+/**
+ * Styled component for info subsection text wrapper.
+ */
+export const TextWrapper = styled.div`
+  margin-left: 2rem;
+  font-size: 1.3em;
+  font-weight: 600;
+`;
+
+/**
+ * Styled component for React-router link with custom styles.
+ */
+export const StyledLink = styled(Link)`
+  color: ${cssVars.colorForegroundPickAux1};
+`;
+
+/**
+ * Styled component for accept button (as a link).
+ */
+export const LinkButton = styled.button`
+  border: none;
+  background: none;
+  outline: none;
+  user-select: none;
+  font-size: 1.4em;
+  font-weight: 600;
+  text-decoration: underline;
+  color: ${cssVars.colorForegroundPickAux1};
 `;
