c20c4df256b5722c927728394326a329fae3fc64
Cirill Usatchoff <usatchoff@gmail.com>
2021-04-01 16:00:33 +0300

    Review entity updated

--- api/src/database/entities/Review.entity.ts
+++ api/src/database/entities/Review.entity.ts
@@ -7,6 +7,7 @@ import {
 } from 'typeorm';
 
 import User from './User.entity';
+import Workplace from './Workplace.entity';
 
 /**
  * Review model.
@@ -16,12 +17,23 @@ export default class Review {
   @PrimaryGeneratedColumn()
   id: number = 0;
 
-  @OneToOne(_ => User) @JoinColumn()
+  @OneToOne(_ => User, { nullable: false }) @JoinColumn()
   author: User = new User();
 
-  @OneToOne(_ => User) @JoinColumn()
+  @OneToOne(_ => User, { nullable: false }) @JoinColumn()
   target: User = new User();
+
+  @OneToOne(_ => Workplace, { nullable: false }) @JoinColumn()
+  workplace: Workplace = new Workplace();
 
   @Column('varchar', { length: 200, nullable: false })
   question1: string = '';
+
+  /**
+   * In progress: 0
+   * Approved:    1
+   * Rejected:   -1
+   */
+  @Column('int', { nullable: false })
+  approved: number = 0;
 }
