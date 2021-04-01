ff4cef3bde355c6a045af58df5303b53fd29d2e8
Cirill Usatchoff <usatchoff@gmail.com>
2021-04-01 16:11:21 +0300

    Workplace migration provided

--- api/src/database/migrations/1617271042859-User updated.ts
+++ api/src/database/migrations/1617271042859-User updated.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UserUpdated1617271042859 implements MigrationInterface {
-    name = 'UserUpdated1617271042859'
+  name = 'UserUpdated1617271042859'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "lastUpdatedAt" TIMESTAMP WITH TIME ZONE NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "user" ADD "lastUpdatedAt" TIMESTAMP WITH TIME ZONE NOT NULL');
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastUpdatedAt"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "user" DROP COLUMN "lastUpdatedAt"');
+  }
 }
