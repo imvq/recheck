ef9334bb66db2f889c4ebc88e174a7bbe9f37799
Cirill Usatchoff <usatchoff@gmail.com>
2021-04-03 10:43:43 +0300

    LinkedIn scraper form layout updated

--- src/components/pages/LinkedInScraperPage/index.tsx
+++ src/components/pages/LinkedInScraperPage/index.tsx
@@ -1,13 +1,35 @@
-import { Wrapper, Form, Input } from './styled';
+import { useHistory } from 'react-router-dom';
+
+import {
+  Wrapper, Form, InputLineWrapper, Input, Button, ButtonWrapper
+} from './styled';
 
 /**
  * Page with single form to scrape linkedIn profile link.
  */
-export default () => (
-  <Wrapper>
-    <Form>
-      <span>lala</span>
-      <Input type='text' />
-    </Form>
-  </Wrapper>
-);
+export default () => {
+  const history = useHistory();
+
+  return (
+    <Wrapper>
+      <Form>
+        <span>Введите ссылку на Ваш публичный профиль LinkedIn</span>
+        <span>Ссылка должна быть в форме</span>
+        <span>https://www.linkedin.com/in/профиль</span>
+        <InputLineWrapper>
+          <Input type='text' />
+          <ButtonWrapper>
+            {/* TODO: Scraping. */}
+            <Button onClick={() => history.push('/profile')}>
+              Подтвердить
+            </Button>
+          </ButtonWrapper>
+        </InputLineWrapper>
+        <span>
+          Предоставляя ссылку, Вы подтверждаете, что являетесь
+          влаельцем данной страницы.
+        </span>
+      </Form>
+    </Wrapper>
+  );
+};
--- src/components/pages/LinkedInScraperPage/styled.tsx
+++ src/components/pages/LinkedInScraperPage/styled.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
-import { cssVars } from 'utils/style.common';
+import { mixins } from 'utils/style.common';
 
 export const Wrapper = styled.div`
+  height: 100%;
   background-color: #fbfbff;
   display: flex;
   justify-content: center;
@@ -14,16 +15,41 @@ export const Form = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 50rem;
+  text-align: center;
+  width: 60rem;
   height: fit-content;
-  padding: 5rem 2rem;
+  box-sizing: border-box;
+  padding: 5rem;
   background-color: white;
   border-radius: 1rem;
-  box-shadow: ${cssVars.sectionShadow};
+  filter: drop-shadow(0 0 2rem rgba(0, 0, 0, .3));
+  font-family: Open Sans;
+`;
+
+export const InputLineWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
 `;
 
 export const Input = styled.input`
   width: 100%;
   padding: 1rem;
   border-radius: .5rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-left: 1rem;
+`;
+
+export const Button = styled.button`
+  ${mixins.DefaultButton};
+
+  height: 100%;
+  background-color: #28a745;
+  color: white;
+  border: none;
+  outline: none;
+  user-select: none;
 `;
