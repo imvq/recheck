ecdef5e8e290463a4c14c549bf2c7bf078f89b85
Cirill Usatchoff <usatchoff@gmail.com>
2021-04-27 15:47:44 +0300

    Minor stuff

--- api/src/controllers/OAuthLinkedIn.ts
+++ api/src/controllers/OAuthLinkedIn.ts
@@ -13,16 +13,6 @@ export default class LinkedInController {
   public constructor(@Inject private readonly injectedService: LinkedInOAuthService) {}
 
   /**
-   * Exchange confirmation code to LinkedIn access token.
-   */
-  @Path('/exchange')
-  @POST
-  public async exchangeLinkedInAuthCode(bodyDta: DTOs.ExchangeLinkedInAuthCodeDto)
-    : Promise<Types.ExchangeLinkedInAuthCodeResponseDto> {
-    return this.injectedService.exchangeLinkedInAuthCode(bodyDta);
-  }
-
-  /**
    * Get LinkedIn profile info.
    */
   @Path('/retrieve')
@@ -30,5 +20,15 @@ export default class LinkedInController {
   public async getLinkedInProfile(@Context context: ServiceContext)
     : Promise<Types.RetrieveLinkedInProfileInfoResponseDto> {
     return this.injectedService.retrieveProfileInfo(context.request.cookies);
+  }
+
+  /**
+   * Exchange confirmation code to LinkedIn access token.
+   */
+   @Path('/exchange')
+   @POST
+  public async exchangeLinkedInAuthCode(bodyDta: DTOs.ExchangeLinkedInAuthCodeDto)
+     : Promise<Types.ExchangeLinkedInAuthCodeResponseDto> {
+    return this.injectedService.exchangeLinkedInAuthCode(bodyDta);
   }
 }
