3b290072064b7f02e55fb99557ee455b34d135d1
Cirill Usatchoff <usatchoff@gmail.com>
2021-04-27 14:01:44 +0300

    Confirmation manager provided

---
+++ api/src/database/managers/ConfirmationManager.ts
@@ -0,0 +1,25 @@
+import { getRepository, Repository } from 'typeorm';
+
+import Types from '@types';
+import User from '../entities/User.entity';
+import Confirmation from '../entities/Confirmation.entity';
+
+/**
+ * Class providing operations with Confirmation entity.
+ */
+export default class ConfirmationManager {
+  private static repo: Types.Nullable<Repository<Confirmation>> = null;
+
+  // Must be invoked after connection being established.
+  public static loadRepository() {
+    ConfirmationManager.repo = getRepository(Confirmation);
+  }
+
+  public static createConfirmation = async (forUser: User, code: number) => (
+    ConfirmationManager.repo?.save(ConfirmationManager.repo.create({ user: forUser, code }))
+  )
+
+  public static getConfirmation = async (id: number) => (
+    ConfirmationManager.repo?.findOne(id)
+  )
+}
