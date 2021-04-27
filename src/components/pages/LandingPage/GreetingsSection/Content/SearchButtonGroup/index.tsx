9b7963fbf4db38d83086044485e59969e8fe16a6
Cirill Usatchoff <usatchoff@gmail.com>
2021-04-27 19:05:42 +0300

    Authorization check provided for the search button

--- src/components/pages/LandingPage/GreetingsSection/Content/SearchButtonGroup/index.tsx
+++ src/components/pages/LandingPage/GreetingsSection/Content/SearchButtonGroup/index.tsx
@@ -1,12 +1,18 @@
 import { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 
-import { setIsSearchPopupVisible } from 'store';
+import { AppState, setPageLocked, setIsLoginPopupVisible, setIsSearchPopupVisible } from 'store';
 import { Wrapper, SearchButton } from './styled';
 import RocketGroup from './RocketGroup';
-import { IProps, IDispatchProps } from './types';
+import { IProps, IStateProps, IDispatchProps } from './types';
+
+const mapStateToProps = (store: AppState): IStateProps => ({
+  isAuthorized: store.auth.isAuthorized
+});
 
 const mapDispatchToProps: IDispatchProps = {
+  lockPage: setPageLocked,
+  setIsLoginPopupVisible,
   setIsSearchPopupVisible
 };
 
@@ -15,10 +21,17 @@ const mapDispatchToProps: IDispatchProps = {
  * and a decorated SVG rocket attached to.
  */
 const SearchButtonGroup: FunctionComponent<IProps> = (props) => (
-  <Wrapper onClick={() => props.setIsSearchPopupVisible(true)}>
+  <Wrapper onClick={
+    props.isAuthorized === null
+      ? props.lockPage
+      : props.isAuthorized
+        ? () => props.setIsSearchPopupVisible(true)
+        : () => props.setIsLoginPopupVisible(true)
+  }
+  >
     <SearchButton onClick={props.onClick} />
     <RocketGroup />
   </Wrapper>
 );
 
-export default connect(null, mapDispatchToProps)(SearchButtonGroup);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchButtonGroup);
--- src/components/pages/LandingPage/GreetingsSection/Content/SearchButtonGroup/types.ts
+++ src/components/pages/LandingPage/GreetingsSection/Content/SearchButtonGroup/types.ts
@@ -1,9 +1,17 @@
+import { Nullable } from 'utils/types.common';
+
 export interface IOwnProps {
   onClick?: (...args: any[]) => void;
 }
 
+export interface IStateProps {
+  isAuthorized: Nullable<boolean>;
+}
+
 export interface IDispatchProps {
+  lockPage(): void;
+  setIsLoginPopupVisible(flag: boolean): void;
   setIsSearchPopupVisible: (newValue: boolean) => void;
 }
 
-export type IProps = IOwnProps & IDispatchProps;
+export type IProps = IOwnProps & IStateProps & IDispatchProps;
