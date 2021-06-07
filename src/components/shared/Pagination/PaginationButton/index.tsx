71e4879b68b6fde3a30bbc6cfe902552d886207c
Cirill Usatchoff <usatchoff@gmail.com>
2021-06-07 16:58:19 +0300

    Pagination style updated

--- src/components/shared/Pagination/PaginationButton/index.tsx
+++ src/components/shared/Pagination/PaginationButton/index.tsx
@@ -1,13 +1,41 @@
+import { ReactComponent as ArrowRightSvg } from 'assets/images/shared/Pagination/ArrowRight.svg';
+import { ReactComponent as ArrowLeftSvg } from 'assets/images/shared/Pagination/ArrowLeft.svg';
+import { PaginationDirection } from 'utils/enums';
 import { IProps } from './types';
-import { Wrapper } from './styled';
+import { Wrapper, ArrowLeftWrapper, ArrowRightWrapper } from './styled';
 
-export default (props: IProps) => (
-  <Wrapper
-    type='button'
-    onClick={() => props.callback(props.page)}
-    disabled={!props.isEnabled}
-    isCurrent={props.isCurrent}
-  >
-    {props.page}
-  </Wrapper>
-);
+const isArrowRight = (page: number | PaginationDirection) => page === PaginationDirection.Next;
+const isArrowLeft = (page: number | PaginationDirection) => page === PaginationDirection.Prev;
+
+export default (props: IProps) => {
+  const ArrowLeft = () => (
+    <ArrowLeftWrapper isEnabled={props.isEnabled}>
+      <ArrowLeftSvg onClick={() => props.callback(props.page)} />
+    </ArrowLeftWrapper>
+  );
+
+  const ArrowRight = () => (
+    <ArrowRightWrapper isEnabled={props.isEnabled}>
+      <ArrowRightSvg onClick={() => props.callback(props.page)} />
+    </ArrowRightWrapper>
+  );
+
+  if (isArrowLeft(props.page)) {
+    return <ArrowLeft />;
+  }
+
+  if (isArrowRight(props.page)) {
+    return <ArrowRight />;
+  }
+
+  return (
+    <Wrapper
+      type='button'
+      onClick={() => props.callback(props.page)}
+      disabled={!props.isEnabled}
+      isCurrent={props.isCurrent}
+    >
+      {props.page}
+    </Wrapper>
+  );
+};
--- src/components/shared/Pagination/PaginationButton/styled.tsx
+++ src/components/shared/Pagination/PaginationButton/styled.tsx
@@ -1,12 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { cssVars } from 'utils/style.common';
 
 export const Wrapper = styled.button<{ isCurrent?: boolean; }>`
-  background-color: ${props => (props.isCurrent ? 'white' : cssVars.colorForegroundPickAux1)};
-  color: ${props => (props.isCurrent ? cssVars.colorForegroundPickAux1 : 'white')};
-  border: ${cssVars.thiknessDefault} ${cssVars.colorForegroundPickAux1} solid;
-  border-radius: 100%;
+  background: none;
+  border: none;
+  color: ${props => (props.isCurrent ? cssVars.colorForegroundPickAux1 : 'black')};
   width: 2rem;
   height: 2rem;
   padding: 1.2rem;
@@ -23,11 +22,13 @@ export const Wrapper = styled.button<{ isCurrent?: boolean; }>`
   &:hover:not(:disabled) {
     opacity: ${cssVars.opacityButtonHover};
     filter: alpha(opacity=${cssVars.opacityButtonHover} * 100);
+    text-decoration: underline;
   }
 
   &:active:not(:disabled) {
     opacity: ${cssVars.opacityButtonActive};
     filter: alpha(opacity=${cssVars.opacityButtonActive} * 100);
+    text-decoration: underline;
   }
 
   &:disabled {
@@ -36,4 +37,25 @@ export const Wrapper = styled.button<{ isCurrent?: boolean; }>`
     background-color: #b6b6b6;
     cursor: default;
   }
+`;
+
+export const Arrow = ({ isEnabled }: { isEnabled?: boolean }) => css`
+  width: 1rem;
+  height: 2.2rem;
+  cursor: ${isEnabled ? 'pointer' : 'default'};
+
+  display: flex;
+  align-items: center;
+  
+  svg>path {
+    fill: ${(isEnabled ? '#33c7ba' : '#c4c4c4')};
+  }
+`;
+
+export const ArrowRightWrapper = styled.div<{ isEnabled?: boolean; }>`
+  ${props => Arrow({ isEnabled: props.isEnabled })}
+`;
+
+export const ArrowLeftWrapper = styled.div<{ isEnabled?: boolean; }>`
+  ${props => Arrow({ isEnabled: props.isEnabled })}
 `;
