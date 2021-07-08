ff1a4566c4c009429b3b6b0ca998e728701970ed
Cirill Usatchoff <usatchoff@gmail.com>
2021-07-08 20:38:05 +0300

    Mail transport configured

--- api/src/services/Mail.ts
+++ api/src/services/Mail.ts
@@ -1,23 +1,37 @@
 import { createTransport, Transporter } from 'nodemailer';
+import nodemailerMailgun from 'nodemailer-mailgun-transport';
 
 import * as generalTypes from '@typing/general';
 import utils from '@utils';
 
 import TemplatesService from './Templates';
 
+function getMailTransport() {
+  if (process.env.NODE_ENV === 'development') {
+    return createTransport({
+      port: parseInt(process.env.MAIL_PORT || ''),
+      secure: utils.parseBoolean(process.env.MAIL_SECURED || ''),
+      host: process.env.MAIL_HOST || '',
+      auth: {
+        user: process.env.MAIL_USERNAME || '',
+        pass: process.env.MAIL_PASSWORD || ''
+      }
+    });
+  }
+
+  return createTransport(nodemailerMailgun({
+    auth: {
+      api_key: process.env.MAIL_API_KEY || '',
+      domain: process.env.MAIL_DOMAIN || ''
+    }
+  }));
+}
+
 /**
  * Tool for sending emails.
  */
 export default class MailService {
-  private static readonly instance: Transporter = createTransport({
-    port: parseInt(process.env.MAIL_PORT as string, 10),
-    secure: utils.parseBoolean(process.env.MAIL_SECURED as string),
-    host: process.env.MAIL_HOST as string,
-    auth: {
-      user: process.env.MAIL_USERNAME as string,
-      pass: process.env.MAIL_PASSWORD as string
-    }
-  });
+  private static readonly instance: Transporter = getMailTransport();
 
   public static async sendConfirmationMail(to: string, confirmationCode: string): Promise<void> {
     const templatesPath = `${__dirname}/templates/confirmation.handlebars`;
