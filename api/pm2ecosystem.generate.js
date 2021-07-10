8ec2a3cabf488afb55d2cbf6767348de585d8a3f
Cirill Usatchoff <usatchoff@gmail.com>
2021-07-10 11:31:32 +0300

    PM2 mode configured

--- api/pm2ecosystem.generate.js
+++ api/pm2ecosystem.generate.js
@@ -8,6 +8,8 @@ module.exports = {
   apps: [{
     name: 'recheck_backend',
     script: './index.js',
+    instances: 1,
+    exec_mode: 'cluster',
     env_production: {
       ${Object.keys(envConfig).map(key => `${key}: '${envConfig[key]}'`).join(',\n      ')}
     }
