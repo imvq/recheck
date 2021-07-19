9efd238b4fe3eeb73f18ab6c0795ae8975471bc3
Cirill Usatchoff <usatchoff@gmail.com>
2021-07-19 19:29:13 +0300

    Side menu styles updated

--- src/components/shared/MainToolbar/styled.tsx
+++ src/components/shared/MainToolbar/styled.tsx
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.aside`
+  position: relative;
   height: 100%;
   background-color: white;
   font-weight: 400;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+  &::after {
+    content: '';
+    position: absolute;
+    height: 100%;
+    right: 0;
+    box-shadow: 0px 0px 0.25rem 0.063rem rgba(0, 0, 0, 0.2);
+  }
 
   display: grid;
   grid-template:
