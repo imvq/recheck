c6cad0e38a42634d0909873338a1c90993d0bc84
Cirill Usachov <usachov@imvq.space>
2021-09-16 00:37:29 +0300

    NameToken entity migration provided

---
+++ api/migrations/sqls/20210915193006-Name-token-entity-provided-down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS public.name_tokens_bounds;
+DROP TABLE IF EXISTS public.name_tokens;
---
+++ api/migrations/sqls/20210915193006-Name-token-entity-provided-up.sql
@@ -0,0 +1,28 @@
+-- Creating table for name tokens.
+CREATE TABLE IF NOT EXISTS public.name_tokens
+(
+    id          bigserial,
+    token_value text       NOT NULL,
+
+    -- Indices for unique fields will be generated automatically.
+    CONSTRAINT nt_unique_token_value UNIQUE(token_value),
+
+    -- Index for the primary key will be generated automatically.
+    CONSTRAINT nt_pk_id PRIMARY KEY(id)
+);
+
+-- Creating table for name tokens bounds.
+CREATE TABLE IF NOT EXISTS public.name_tokens_bounds
+(
+    name_token_id bigserial,
+    user_id       bigserial,
+
+    -- Index for the primary key will be generated automatically.
+    CONSTRAINT ntb_pk_id         PRIMARY KEY(name_token_id, user_id),
+    CONSTRAINT ntb_fk_name_token FOREIGN KEY(name_token_id) REFERENCES public.name_tokens(id),
+    CONSTRAINT ntb_fk_user       FOREIGN KEY(user_id)       REFERENCES public.users(id)
+);
+
+-- Setting owner.
+ALTER TABLE public.name_tokens OWNER TO recheck;
+ALTER TABLE public.name_tokens_bounds OWNER TO recheck;
