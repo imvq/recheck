836225215216aabefa992403a14a62eab9fc86fd
Cirill Usachov <usachov@imvq.space>
2021-11-13 15:56:39 +0200

    Users availability dependencies updated

--- api/migrations/sqls/20211113113757-Users-availability-table-provided-up.sql
+++ api/migrations/sqls/20211113113757-Users-availability-table-provided-up.sql
@@ -1,12 +1,12 @@
 -- Creating table for users available.
 CREATE TABLE IF NOT EXISTS public.users_available (
-    owner_id  BIGSERIAL,
-    target_id BIGSERIAL,
+    owner_private_token UUID,
+    target_shareable_id UUID,
 
     -- Index for the primary key will be generated automatically.
-    CONSTRAINT usr_avl_pk_id     PRIMARY KEY(owner_id, target_id),
-    CONSTRAINT usr_avl_owner_id  FOREIGN KEY(owner_id)  REFERENCES public.users(id),
-    CONSTRAINT usr_avl_target_id FOREIGN KEY(target_id) REFERENCES public.users(id)
+    CONSTRAINT usr_avl_pk_id       PRIMARY KEY(owner_private_token, target_shareable_id),
+    CONSTRAINT usr_avl_owner_pt    FOREIGN KEY(owner_private_token) REFERENCES public.users(private_token),
+    CONSTRAINT usr_avl_target_shid FOREIGN KEY(target_shareable_id) REFERENCES public.users(shareable_id)
 );
 
 -- Setting owner.
