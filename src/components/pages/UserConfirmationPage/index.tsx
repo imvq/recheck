6cb8791866fe36461fe507f0cfa43a047a6c8082
Cirill Usachov <usachov@imvq.space>
2021-11-19 17:55:28 +0200

    Confirmation page updated

--- src/components/pages/UserConfirmationPage/index.tsx
+++ src/components/pages/UserConfirmationPage/index.tsx
@@ -1,30 +1,19 @@
 import { useEffect } from 'react';
-import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import { jumpTo } from 'commons/utils/misc';
 import { apiClient } from 'commons/utils/services';
-import { AppState } from 'store';
 
-import * as types from './types';
-
-const mapStateToProps = (store: AppState): types.IStateProps => ({
-  isAuthorized: store.profile.isAuthorized
-});
-
-function UserConfirmationPage(props: types.IProps) {
+function UserConfirmationPage() {
   const { uuid: pageUuid } = useParams<{ uuid: string }>();
 
   useEffect(() => {
-    // Perform confirmation only after the authorization check.
-    if (props.isAuthorized !== null) {
-      apiClient.completeRegistration({ confirmationCode: pageUuid })
-        .then(() => jumpTo(props.isAuthorized ? '/profile' : '/'))
-        .catch(() => jumpTo('/404'));
-    }
-  }, [props.isAuthorized]);
+    apiClient.completeRegistration(pageUuid)
+      .then(() => window.location.replace(window.location.origin))
+      .catch(() => jumpTo('/404'));
+  }, []);
 
   return null;
 }
 
-export default connect(mapStateToProps)(UserConfirmationPage);
+export default UserConfirmationPage;
--- src/components/pages/UserConfirmationPage/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IOwnProps {}
-
-export interface IStateProps {
-  isAuthorized: boolean | null;
-}
-
-export interface IDispatchProps {}
-
-export type IProps = IOwnProps & IStateProps & IDispatchProps;
