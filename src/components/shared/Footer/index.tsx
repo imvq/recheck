5ef3ad275f9cfedfd0bcef00dd02e0267ace6cc2
Cirill Usachov <usachov@imvq.space>
2022-01-13 23:06:33 +0200

    Footer updated

--- src/components/shared/Footer/index.tsx
+++ src/components/shared/Footer/index.tsx
@@ -1,37 +1,28 @@
-import { Link as RouterLink } from 'react-router-dom';
+import { memo } from 'react';
 
 import * as styled from './styled';
 
-/**
- * The footer.
- */
-export default () => (
-  <styled.Wrapper>
-    <styled.SectionWrapper>
-      <styled.InnerLogo />
-    </styled.SectionWrapper>
+function Footer() {
+  return (
+    <styled.Wrapper>
+      <styled.SectionWrapper>
+        <styled.InnerLogo />
+      </styled.SectionWrapper>
 
-    <styled.SectionWrapper>
-      <styled.MenuEntryTextMarked>Меню:</styled.MenuEntryTextMarked>
-      <RouterLink style={{ textDecoration: 'none', color: 'black' }} to='/#HowToWorkTitle'>
-        <styled.MenuEntryText>О платформе</styled.MenuEntryText>
-      </RouterLink>
-      <RouterLink style={{ textDecoration: 'none', color: 'black' }} to='/#InfoblockTitle'>
-        <styled.MenuEntryText>Преимущества</styled.MenuEntryText>
-      </RouterLink>
-    </styled.SectionWrapper>
+      <styled.SectionWrapper>
+        <styled.MenuEntryTextMarked>Почта:</styled.MenuEntryTextMarked>
+        <styled.MenuEntryTextDisabled>info@recheck-candidate.com</styled.MenuEntryTextDisabled>
+      </styled.SectionWrapper>
 
-    <styled.SectionWrapper>
-      <styled.MenuEntryTextMarked>Почта:</styled.MenuEntryTextMarked>
-      <styled.MenuEntryTextDisabled>info@recheck-candidate.com</styled.MenuEntryTextDisabled>
-    </styled.SectionWrapper>
+      <styled.SectionWrapper>
+        <styled.MenuEntryTextMarked>Соц. сети:</styled.MenuEntryTextMarked>
+        <styled.SocialLink href='/'>
+          <styled.TelegramIconWrapper />
+        </styled.SocialLink>
+      </styled.SectionWrapper>
+      <styled.EmptyWrapper />
+    </styled.Wrapper>
+  );
+}
 
-    <styled.SectionWrapper>
-      <styled.MenuEntryTextMarked>Соц. сети:</styled.MenuEntryTextMarked>
-      <styled.SocialLink href='/'>
-        <styled.TelegramIconWrapper />
-      </styled.SocialLink>
-    </styled.SectionWrapper>
-    <styled.EmptyWrapper />
-  </styled.Wrapper>
-);
+export default memo(Footer);
