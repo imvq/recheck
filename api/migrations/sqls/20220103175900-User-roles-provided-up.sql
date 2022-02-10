74f701525ab31cae0801a2e08de75a9bd2e16ada
Cirill Usachov <usachov.ciri@gmail.com>
2022-02-10 21:09:49 +0200

    Role typo fixed

--- api/migrations/sqls/20220103175900-User-roles-provided-up.sql
+++ api/migrations/sqls/20220103175900-User-roles-provided-up.sql
@@ -1,4 +1,4 @@
-CREATE TYPE ROLE AS ENUM ('recruiter', 'randidate');
+CREATE TYPE ROLE AS ENUM ('recruiter', 'candidate');
 
 ALTER TABLE public.users
     ADD COLUMN last_role ROLE DEFAULT 'recruiter';
